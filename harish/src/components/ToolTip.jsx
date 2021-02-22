9c348455832fe2bdd62afc436588cd5340419e67
harish-ponna-au6 <harishponna@codalyze.com>
2021-02-22 19:13:20 +0530

    added pdf component with onclick

--- harish/src/components/ToolTip.jsx
+++ harish/src/components/ToolTip.jsx
@@ -1,21 +1,32 @@
 import React from "react";
 
 const ToolTip = (props) => {
-  const { left, top, width, height, canvasWidth, canvasHeight } = props;
+  const {
+    Text: text,
+    Geometry: {
+      BoundingBox: { Left, Top, Width, Height },
+    },
+    canvasWidth,
+    canvasHeight,
+    handleClick,
+  } = props;
 
   const style = {
     position: "absolute",
-    left: left * canvasWidth - 3,
-    top: top * canvasHeight - 3,
-    width: width * canvasWidth + 3,
-    height: height * canvasHeight + 3,
+    left: Left * canvasWidth - 3,
+    top: Top * canvasHeight - 3,
+    width: Width * canvasWidth + 3,
+    height: Height * canvasHeight + 3,
     border: "1px dashed red",
     background: "#2f2e2e40",
     zIndex: 999,
-    transitionDuration: '400ms'
   };
 
-  return <div style={{ ...style }}></div>;
+  console.log({ Width, Height, canvasHeight });
+
+  return (
+    <div style={{ ...style }} onClick={(_) => handleClick(text)}></div>
+  );
 };
 
 export default ToolTip;
